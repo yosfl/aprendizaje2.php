f8a4b8325ec11d2e6ab65855cb0f150ece60f8d4
yosfl <florezyosuarplay@gmail.com>
2024-11-13 20:44:12 -0500

    13noviembre2024

---
+++ base_de_datos.php
@@ -0,0 +1,12 @@
+REATE TABLE users(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(20) NOT NULL,
+    second_name VARCHAR(20) NOT NULL,
+    lastname VARCHAR (20) NOT NULL,
+    second_lastname VARCHAR (20) NOT NULL,
+    document BIGINT (10) UNIQUE NOT NULL,
+    password  VARCHAR (100)  NULL,
+    email VARCHAR (50) NULL,
+    phone BIGINT(10) NULL,
+    age INT (2) NULL
+ );
